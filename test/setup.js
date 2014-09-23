4c18cf07c6f3db3055340ebcc4563141f1bd01ee
Martin Middel <martin.middel@liones.nl>
2014-09-23 15:58:25 +0200

    Update setup.js

--- test/setup.js
+++ test/setup.js
@@ -10,18 +10,6 @@ require.config({
 	},
 
 	"packages": [
-		{
-			"name": "whynot",
-			"location": "../test/vendor/whynot/src"
-		},
-		{
-			"name": "fontoxml-dom-utils",
-			"location": "../test/vendor/fontoxml-dom-utils/src"
-		},
-		{
-			"name": "slimdom",
-			"location": "../test/vendor/slimdom/lib"
-		}
 	],
 
 	"shim": {
